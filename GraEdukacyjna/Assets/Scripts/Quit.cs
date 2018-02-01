503898a5367232ae3b8a1623cd5a6fdf7cd2622a
krystynastepien <krystyna.stepien91@gmail.com>
2018-02-01 16:48:20 +0100

    menu wyglad
    przycisk quit

---
+++ GraEdukacyjna/Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+    public void quit_app()
+    {
+        Debug.Log("Has quit game");
+        Application.Quit();
+    }
+}

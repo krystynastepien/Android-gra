f3a8f3462a9499879b92d3baef292d8bf3f7a3c7
joannabanaszak <32677207+joannabanaszak@users.noreply.github.com>
2018-02-01 17:37:07 +0100

    Zliczanie punktów i menu przegranej
    plus przycisk zagraj ponownie

---
+++ GraEdukacyjna/Assets/Scripts/LoseGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseGame : MonoBehaviour {
+
+    public Text currentScoreText;
+    public Text highScoreText;
+    
+    public void Start()
+    {
+        currentScoreText.GetComponent<Text>().text ="SCORE "+ GameValues.currentScore.ToString();
+        highScoreText.GetComponent<Text>().text = "HIGH SCORE "+ PlayerPrefs.GetInt("HIGH_SCORE", 0).ToString();
+
+    }
+
+	public void onPlayAgainButtonClick()
+    {
+        Application.LoadLevel("Dodawanie");
+        
+    }
+		
+    public void onMenuButtonClick()
+    {
+
+    }
+
+	
+}
